232c0a96084fea96ab46837a23c77def6b298652
Dagmar und Peter <peterbuzek@PeterBuzeksMBP.fritz.box>
2014-11-15 11:47:12 +0100

    second rev of project

---
+++ LemonadeStand/Price.swift
@@ -0,0 +1,9 @@
+//
+//  Price.swift
+//  LemonadeStand
+//
+//  Created by Dagmar und Peter on 03.11.14.
+//  Copyright (c) 2014 PB King. All rights reserved.
+//
+
+import Foundation
---
+++ LemonadeStand/Supplies.swift
@@ -0,0 +1,9 @@
+//
+//  Supplies.swift
+//  LemonadeStand
+//
+//  Created by Dagmar und Peter on 03.11.14.
+//  Copyright (c) 2014 PB King. All rights reserved.
+//
+
+import Foundation
